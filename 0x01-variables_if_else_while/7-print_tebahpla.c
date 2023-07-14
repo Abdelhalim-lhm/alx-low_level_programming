86a7ad497bc2e3997d7eb935ad2bfe8e5d2e44dd
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-14 14:33:53 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'task N°1'
+ * 0. Positive anything is better than negative nothing
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha <= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+		printf("\n");
+	return (0);
+}
