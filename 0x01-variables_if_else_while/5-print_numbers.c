f089d163db9f244348fe0940b9e33765fe0c8f0d
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-14 14:19:04 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'task N°5'
+ * Numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+
+	}
+		printf("\n");
+	return (0);
+}
