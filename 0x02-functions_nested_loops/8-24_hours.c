7caf4c7f9c0883c728373e4387e7489b6db10dab
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-18 14:34:55 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that print the 24 hours of a day
+ * Return: 0 Succes
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2 ; i++)
+	{
+		for (j = 0 ; j <= 3; j++)
+		{
+			for (k = 0 ; k <= 5; k++)
+			{
+				for (l = 0 ; l <= 9; l++)
+				{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(58);
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+				}
+			}
+		}
+	}
+}
