1b6651e3ad01a899512391a8fd2effb885e5ee6a
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-19 13:17:28 -0700

    THE LAST

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h>
 /**
  * main - entrey
  * function that print the first febonaci terms
@@ -7,27 +8,42 @@
 
 int main(void)
 {
-	int i;
-	unsigned long int febo, f1, f2;
+	int i, j;
+	unsigned long feb, f1, f2, fo1, fo2, febo, num;
 
-	febo = 0;
+	feb = 0;
 	f1 = 1;
 	f2 = 2;
-	printf("%lu, %lu,", f1, f2);
-		for (i = 1 ; i <= 96 ; i++)
+	fo1 = 0;
+	fo2 = 0;
+	febo = 0;
+	num = 10000000;
+		for (i = 1 ; i <= 98 ; i++)
 		{
-			febo = f1 + f2;
-			f1 = f2;
-			f2 = febo;
-		if (i < 96)
-		{
-			printf(" %lu,", febo);
+			if (fo1 > 0)
+				printf("%lu", fo1);
+			j = (floor(log10(num)) + 1) - 1 - (floor(log10(f1)) + 1);
+				while (fo1 > 0 && j > 0)
+				{
+					printf("%d", 0);
+					j--;
+				}
+				printf("%lu", f1);
+
+				feb = (f1 + f2) % num;
+				febo = fo1 + fo2 + (f1 + f2) / num;
+				f1 = f2;
+				f2 = feb;
+				fo1 = fo2;
+				fo2 = febo;
+				if (i != 98)
+				{
+					printf(", ");
+				}
+				else
+				{
+					printf("\n");
+				}
 		}
-		else if (i == 96)
-		{
-			printf(" %lu", febo);
-		}
-		}
-	printf("\n");
 	return (0);
 }
