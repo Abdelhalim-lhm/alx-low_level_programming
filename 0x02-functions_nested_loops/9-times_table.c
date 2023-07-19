540c524c2a42f5718cdabfceeca16720ad096fc4
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-18 18:53:24 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,32 +14,32 @@ void times_table(void)
 			{
 				if (k >= 10 && j < 9)
 				{
-					putchar((k / 10) + 48);
-					putchar((k % 10) + 48);
-					putchar(',');
-					putchar(' ');
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
 				}
 				else if (k < 10 && j < 9)
 				{
-					putchar(k + 48);
-					putchar(',');
-					putchar(' ');
+					_putchar(k + 48);
+					_putchar(',');
+					_putchar(' ');
 					if (k + i < 10)
 					{
-						putchar(' ');
+						_putchar(' ');
 					}
 				}
 				else if (j == 9 && k < 10)
 				{
-					putchar(k + 48);
+					_putchar(k + 48);
 				}
 				else if (j == 9 && k >= 10)
 				{
-					putchar((k / 10) + 48);
-					putchar((k % 10) + 48);
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
 				}
 			k = k + i;
 			}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
