bfd3f36f78b629b709fa906486522f4ccaae4bfd
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-24 13:28:01 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that reset int entry to 98
+ * @n: is the integer to be reset to 98
+ * Return: 0 on succes
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
