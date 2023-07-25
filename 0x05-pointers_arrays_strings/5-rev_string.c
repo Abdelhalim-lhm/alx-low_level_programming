d3f0c4131b1f660976a9302de3309302322cd6d1
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-25 08:30:44 -0700

    5. A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - function that reverse a string
+ * @s : the string to reverse
+ * Return: 0 on succes
+ */
+void rev_string(char *s)
+{
+	char t[100];
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1 ;j >= 0 ; j--)
+	{
+		t[i - j - 1] = s[j];
+	}
+	t[i] = '\0';
+	for (k = 0; k < i; k++)
+	{
+		s[k] = t[k];
+	}
+}
