759be60b681be0bb04810f84aba07cc26eb76945
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-27 14:45:24 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *rot13 - rot13 function
+ * @c: the string to change
+ * Return: 0 on success
+ */
+char *rot13(char *c)
+{
+	int j, i = 0;
+	char l[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char t[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	while (c[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == l[j])
+			{
+				c[i] = t[j];
+			}
+		}
+	i++;
+	}
+	return (c);
+}
