cbdf55672a934ae8a7d1177922ecf183483e9a1c
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-07-31 15:02:59 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: string to check
+ * @needle: set of byte to find
+ * Return: 0 On success
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, n = 0;
+
+	while (needle[n] != '\0')
+	{
+		n++;
+	}
+	while (*haystack != '\0')
+	{
+		int j = 0;
+
+		for (i = 0; i < n; i++)
+		{
+			if (haystack[i] != needle[i])
+			{
+				break;
+			}
+		j++;
+			if (j == n)
+			{
+				return (haystack);
+			}
+		}
+	haystack++;
+	}
+	return (NULL);
+}
