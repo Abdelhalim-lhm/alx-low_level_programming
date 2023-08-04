1d680f68d8952dfb1de371ff4dbf784e3b2155db
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-04 15:11:11 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc <= 2 || argc > 3 )
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 }
