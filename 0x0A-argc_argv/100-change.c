6329701489c01bab9966d5360fddad9f68240aa1
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-04 18:10:32 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -58,6 +58,11 @@ int main(int argc, char *argv[])
 
 	if (argc == 2)
 	{
+		if (atoi(argv[1]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 		if (atoi(argv[1]) < 0)
 		{
 			printf("0\n");
