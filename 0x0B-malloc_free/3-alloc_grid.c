ec4fcd9c7fa574919961f17b33f6a15fda83d373
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-08 12:31:35 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < hight; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
 
