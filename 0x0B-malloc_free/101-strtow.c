1fa07cdbaa91016b57b1c81c1886045f6fcf4262
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-09 11:56:03 -0700

    6. I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strtow - function that splits a string into words
+ * @str: the string to split
+ * Return: 0 on success
+ */
+char **strtow(char *str)
+{
+	int i, j, k,  count = 0;
+	char *st;
+	char **out_st;
+
+	if (str == NULL || strcmp(str, "") == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			count++;
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				i++;
+			}
+		}
+	}
+	out_st = (char **)malloc((count + 1) * sizeof(char *));
+	if (out_st == NULL)
+	{
+		return (NULL);
+	}
+	st = (char *)malloc(count + 1);
+	if (st == NULL)
+	{
+		free(out_st);
+		return (NULL);
+	}
+	*out_st = st;
+	i = 0;
+
+	while (*str != '\0')
+	{
+		if (*str != ' ')
+		{
+			j = 0;
+			while (*str != ' ' && *str != '\0')
+			{
+				st[j++] = *str;
+				str++;
+			}
+			st[j] = '\0';
+			out_st[i++] = st;
+			st = (char *)malloc(count + 1);
+			if (st == NULL)
+			{
+				for (k = 0; k < i; k++)
+				{
+					free(out_st[k]);
+				}
+			free(out_st);
+			return (NULL);
+			}
+		}
+		else
+		{
+			str++;
+		}
+	}
+	out_st[i] = NULL;
+	return (out_st);
+}
