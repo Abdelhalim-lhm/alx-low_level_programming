661be56765b3eef2d4b0fc31d5f7a3be102d9223
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-14 11:03:05 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,8 +64,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(Ndog);
 		return (NULL);
 	}
-	Ndog->name = _strcpy(Ndog->name, name);
+	_strcpy(Ndog->name, name);
 	Ndog->age = age;
-	Ndog->owner = _strcpy(Ndog->owner, owner);
+	_strcpy(Ndog->owner, owner);
 	return (Ndog);
 }
