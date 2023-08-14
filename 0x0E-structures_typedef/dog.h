4f6bc208b291159707a14b9fcd8be113362d3def
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-14 05:23:22 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dogs infos function
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
