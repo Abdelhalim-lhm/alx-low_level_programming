2e73ffb92ce20e67b1f227e3efd1c4d0e2d3367b
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-16 11:27:19 -0700

    4. Most hackers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that print the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: arguments vectors
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	for (i = atoi(argv[1]) - 1; i >= 0; i--)
+	{
+		if (i == 0)
+		{
+			printf("%02hhx\n", arr[i]);
+		}
+		else
+		{
+			printf("%02hhx ", arr[i]);
+		}
+	}
+	return (0);
+}
