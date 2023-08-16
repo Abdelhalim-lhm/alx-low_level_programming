3ed00247761e65f73a9ae7434783cceaf0a1c410
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-16 03:52:16 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that print name
+ * @f: pointer to a function
+ * @name: name to print
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
