5c22e99ecb01e977ef43466efc7bc2645566f291
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-28 11:03:57 -0700

    4. Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,6 +6,8 @@
  */
 void free_listint(listint_t *head)
 {
+	if (head == NULL)
+		return;
 	while (head != NULL)
 	{
 		listint_t *temp = head;
@@ -13,5 +15,4 @@ void free_listint(listint_t *head)
 		free(temp);
 		head = head->next;
 	}
-
 }
