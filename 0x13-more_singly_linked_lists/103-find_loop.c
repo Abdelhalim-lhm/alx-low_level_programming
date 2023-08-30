91bb94284ecb3992e141ca51901ac2b8874ce5e2
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-30 13:01:28 -0700

    14. Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,13 +6,13 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *temp1 = NULL, *temp2 = NULL;
+	listint_t *temp1, *temp2;
 
 	if (head == NULL)
 		return (NULL);
 	temp1 = head;
 
-	while (temp1 != NULL)
+	while (temp1 != NULL && temp->next != NULL )
 	{
 		temp2 = temp1->next;
 		while (temp2 != NULL)
