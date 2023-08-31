1ba86478f4c748c96139f7a5c57821597e0d4c4d
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-08-31 14:06:15 -0700

    1. 1 int to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to convert
+ * Return: the converted number in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0, bit = 0;
+
+	while (i < 32)
+	{
+		if ((n >> i) & 1)
+			bit = i;
+		i++;
+	}
+	for (i = bit; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
