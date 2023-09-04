1324d5e3c0e878ab10a2a75384b07ae8880c5c9b
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-04 16:47:51 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+/**
+ * main - function that cp a file into a another
+ * @ac: number of argument
+ * @av: file arguments
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int FD_VALUE, fn, fn2, output;
+	char string[1024 * 4];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+	fn2 = open(av[1], O_RDONLY);
+	if (fn2 == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
+	fn2 = open(av[1], O_RDONLY);
+	fn = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fn == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	output = read(fn2, string, sizeof(string));
+	if (output == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
+	output = write(fn, string, output);
+	if (output == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(98);
+	}
+	FD_VALUE = close(fn);
+	if (FD_VALUE == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", errno);
+		exit(98);
+	}
+	return (0);
+}
