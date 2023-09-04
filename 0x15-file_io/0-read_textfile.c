0a8e1d1933970b967e036ab34e7e7c86db33e2cb
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-04 13:54:22 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile -  function that reads a text file
+ * and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: number of lettres to be read
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fn;
+	char string[1024];
+	ssize_t output, count;
+
+	fn = open(filename, O_RDONLY);
+	if (fn == -1)
+		return (0);
+	
+	count = read (fn, string, letters);
+	if (count == -1)
+	{
+		close(fn);
+		return (0);
+	}
+	output = write (STDOUT_FILENO, string, count);	
+	close(fn);
+	if (output == -1)
+	{
+		return (0);
+	}
+	return (output);
+}
