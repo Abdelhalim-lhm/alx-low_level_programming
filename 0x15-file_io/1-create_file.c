853238e43892448f66e3c1bceb5edc10d53bcdd5
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-04 15:02:15 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fn == -1)
 		return (-1);
 	output = write(fn, text_content, sizeof(text_content));
@@ -20,5 +20,6 @@ int create_file(const char *filename, char *text_content)
 		close(fn);
 		return (-1);
 	}
+
 	return (1);
 }
