1565cfdb3a802e6182ba11be18d1c12ba32f21c8
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-04 17:34:25 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@
 int main(int ac, char **av)
 {
 	int FD_VALUE, FD_VALUE2, fn, fn2, output;
-	char string[1024 * 4];
+	char string[1024 * 8];
 
 	if (ac != 3)
 	{
@@ -21,13 +21,13 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fn2 = open(av[1], O_RDONLY);
-	if (fn2 == -1)
+	if (fn2 == -1 || !av[1])
 	{
 		dprintf(2, "Error: Can't read from %s\n", av[1]);
 		exit(98);
 	}
 	fn = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fn == -1)
+	if (fn == -1 || !av[2])
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
