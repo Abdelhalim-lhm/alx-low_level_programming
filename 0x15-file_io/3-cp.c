272579ca5945fb9b4e285cff40a91a2eba2a23ec
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-04 17:59:05 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int ac, char **av)
 	FD_VALUE2 = close(fn2);
 	if (FD_VALUE == -1 || FD_VALUE2 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fn);
+		dprintf(2, "Error: Can't close fd %i\n", -1);
 		exit(100);
 	}
 	return (0);
