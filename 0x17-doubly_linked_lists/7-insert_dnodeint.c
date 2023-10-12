45b8714b734e2ca240b6ce7567f2ef1f1b7f43c4
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-12 14:17:21 -0700

    7. Insert at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function that inserts a new node
+ * at a given position
+ * @h: node to add
+ * @idx: position of the node
+ * @n: data of the node
+ * Return: adress of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t *));
+	dlistint_t *current = *h;
+	unsigned int count = 0;
+
+	if (!new_node)
+		return (NULL);
+	if (*h == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if ((*h)->next != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	while (current != NULL)
+	{
+		if (count == idx)
+		{
+			new_node->prev = current->prev;
+			new_node->next = current;
+			if (current->next != NULL)
+			{
+				current->prev->next = new_node;
+			}
+			current->prev = new_node;
+			return (new_node);
+		}
+		count++;
+		current = current->next;
+	}
+	return (NULL);
+}
