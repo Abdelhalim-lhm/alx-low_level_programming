cca36d8c21a51750ba63bdaea9832a37934ccae3
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-03 09:22:47 -0700

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: The hash table
+ * Return: print hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i, fp = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (fp == 1)
+			{
+				printf(", ");
+			}
+			fp = 1;
+			printf("'%s' : '%s'", temp->key, temp->value);
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
